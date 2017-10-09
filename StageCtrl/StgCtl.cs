64c14893ccbbcc87e4c825c88fb8bac50a13c57a
Dorel Guzun <DGuzun@datarecognitioncorp.com>
2017-10-08 20:23:35 -0500

    Starting a ZScan project

---
+++ StageCtrl/StgCtl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+// Strategy pattern.
+// Common "aspects" have been extracted into an abstract class
+// The stage uses a port for communication that has to be configured during initialization.
+// The communication port/device 
+namespace StageCtrl
+{
+    public interface ICommDev
+    {
+        void SetPort(string portName);
+    }
+    public abstract class TranStage
+    {
+        private ICommDev _commDev;
+
+        public abstract void Initialize();
+        public abstract void Move();
+        public abstract void Stop();
+        public abstract double GetSetPosition();
+        public abstract double GetActualPosition();
+        public abstract double GetSetVelocity();
+        public abstract double GetActualVelocity();
+        public abstract double GetAcceleration();
+        public abstract double SetSetPosition();
+        public abstract double SetActualPosition();
+        public abstract double SetSetVelocity();
+        public abstract double SetActualVelocity();
+        public abstract double SetAcceleration();
+    }
+}
---
+++ StageCtrl/TL300.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StageCtrl
+{
+    public class TL300 : TranStage
+    {
+        public TL300()
+        {
+
+        }
+        public override double GetAcceleration()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetActualPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetActualVelocity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetSetPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetSetVelocity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double SetAcceleration()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double SetActualPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double SetActualVelocity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double SetSetPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double SetSetVelocity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
