c27ebecbebd1abebc94b7488660336ee7b09c1c1
Dorel Guzun <DGuzun@datarecognitioncorp.com>
2017-10-15 22:41:52 -0500

    Added a console app to test the funcs from the library

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // test driver - simulation of functions that go into a z-scan app
+        }
+    }
+}
